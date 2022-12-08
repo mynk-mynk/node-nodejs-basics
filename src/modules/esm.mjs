72caa3dc299964a764c630efb36ec3883415276d
Amir Romashkin <my.name.is.amirk@gmail.com>
2022-12-08 18:11:22 +0100

    feat: implement solution for modules

---
+++ src/modules/esm.mjs
@@ -0,0 +1,41 @@
+import { dirname, sep } from "node:path";
+import { fileURLToPath } from "url";
+import { release, version } from "os";
+import { createServer } from "http";
+import "./files/c.js";
+import { default as a } from "./files/a.json" assert { type: "json" };
+import { default as b } from "./files/b.json" assert { type: "json" };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const random = Math.random();
+
+export let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = a;
+} else {
+  unknownObject = b;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+export const myServer = createServer((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
