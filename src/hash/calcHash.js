3b592b1c0cc08bdb53c42bc9af33a531728fb9c0
Amir Romashkin <my.name.is.amirk@gmail.com>
2022-12-08 18:34:23 +0100

    feat: implement solution for hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { readFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
+
+const pathToFile = "src/hash/files/";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileBuffer = await readFile(pathToFile + "fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+  
+  hash.update(fileBuffer);
+  
+  const hex = hash.digest("hex");
+  console.log(hex);
 };
 
 await calculateHash();
