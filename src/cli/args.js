51a162c42571fdba13da232ab6517a44c1199ee2
Amir Romashkin <my.name.is.amirk@gmail.com>
2022-12-08 17:11:12 +0100

    feat: implement solution for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const filtredArgs = process.argv.slice(2);
+  filtredArgs.forEach((arg, i, arr) => {
+    if (i % 2 === 0) {
+      console.log(arg.slice(2), "is", arr[i + 1]);
+    }
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVars = Object.keys(process.env).filter((key) =>
+    key.startsWith("RSS_")
+  );
+  rssVars.forEach((item) => console.log(item + "=" + process.env[item]));
 };
 
 parseEnv();
